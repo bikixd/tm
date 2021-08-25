9c6609b1fefff89830f34a5d35233dc711f966f1
bikixd <89482735+bikixd@users.noreply.github.com>
2021-08-25 23:49:08 +0530

    Add files via upload

---
+++ js/template-js.js
@@ -0,0 +1,8 @@
+function glow() {
+  document.getElementById("toggler").style.backgroundColor =
+    "rgb(255, 255, 255, 0.5)";
+}
+function blour() {
+  document.getElementById("toggler").style.backgroundColor =
+    "rgb(255, 255, 255, 0)";
+}
